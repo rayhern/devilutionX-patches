1f2092f0fdcf13d609ad07b4ebf16728a19b10de
ray <bizong@gmail.com>
2021-01-17 21:56:34 -0800

    update

--- buildRG350.sh
+++ buildRG350.sh
@@ -8,6 +8,5 @@ cd devilutionX
 # Patches are compatible with this version.
 git checkout 5408576f2a86bde0139a5f4d3b7297c7598cc921
 git apply ../patches/add-ip-phonebook.patch
-cd build/
-cmake -DBINARY_RELEASE=ON -DDEBUG=OFF -DPREFILL_PLAYER_NAME=ON ..
-make -j$(nproc)
+cp Packaging/resources/icon.png Packaging/resources/Diablo_32.png
+Packaging/OpenDingux/build.sh rg350
