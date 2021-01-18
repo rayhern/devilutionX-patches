561fd3ae6a2e59aff793a9fb873aa94a03ed0c8b
rayhern <bizong@gmail.com>
2021-01-18 05:34:56 -0800

    updated patch names.

--- buildMacOS.sh
+++ buildMacOS.sh
@@ -7,13 +7,15 @@ cd devilutionX
 # git version 1.1.0 of devilutionX. Patches are compatible with this version.
 git checkout 5408576f2a86bde0139a5f4d3b7297c7598cc921
 # Add patch to add phonebook to diablo.ini file.
-git apply ../patches/add-ip-phonebook.patch
+git apply ../patches/fix-ip-phonebook.patch
 # Add more players to multiplayer games
-git apply ../patches/add-more-players.patch
+git apply ../patches/more-players.patch
 # Allow blacksmith to have a bigger spending limit, and more premium items.
-git apply ../patches/fix-blacksmith-more-premium.patch
+git apply ../patches/qol-blacksmith-more-premium.patch
 # Increase the max amount of gold in each stack type: small, medium, large.
 git apply ../patches/increase-gold-stack-limit.patch
+# Patch to highlight items on ground.
+git apply ../patches/highlight-items-floor.patch
 cd build/
 cmake -DBINARY_RELEASE=ON -DDEBUG=OFF -DPREFILL_PLAYER_NAME=ON ..
 cmake --build . -j $(sysctl -n hw.physicalcpu)
