86b82b5b506495fa6a0acebbdebeef778008fb06
rayhern <bizong@gmail.com>
2021-01-17 18:52:47 -0800

    Build patcher and helper files.

---
+++ buildLinux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# remove old build
+rm -rf devilutionX
+# clone devilutionX
+git clone https://github.com/diasurgical/devilutionX.git
+cd devilutionX
+# git version 1.1.0 of devilutionX. 
+# Patches are compatible with this version.
+git checkout 5408576f2a86bde0139a5f4d3b7297c7598cc921
+git apply ../patches/add-ip-phonebook.patch
+cd build/
+cmake -DBINARY_RELEASE=ON -DDEBUG=OFF ..
+make -j$(nproc)
