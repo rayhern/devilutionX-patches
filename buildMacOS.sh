5f2985d7c05811c1a4cd0e6613ea86f30e8a64a8
rayhern <bizong@gmail.com>
2021-01-19 16:08:03 -0800

    Cleaning up build scripts.

--- buildMacOS.sh
+++ buildMacOS.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+$GIT_BRANCH = "5408576f2a86bde0139a5f4d3b7297c7598cc921"
 # remove old build
 rm -rf devilutionX
 # clone devilutionX
 git clone https://github.com/diasurgical/devilutionX.git
 cd devilutionX
 # git version 1.1.0 of devilutionX. Patches are compatible with this version.
-git checkout 5408576f2a86bde0139a5f4d3b7297c7598cc921
+git checkout $GIT_BRANCH
 clear
 echo "Applying patches..."
 # Add patch to add phonebook to diablo.ini file.
--- buildRG350.sh
+++ buildRG350.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+$GIT_BRANCH = "5408576f2a86bde0139a5f4d3b7297c7598cc921"
+
 # remove old build
 rm -rf devilutionX
 # clone devilutionX
@@ -6,7 +9,8 @@ git clone https://github.com/diasurgical/devilutionX.git
 cd devilutionX
 # git version 1.1.0 of devilutionX. 
 # Patches are compatible with this version.
-git checkout 5408576f2a86bde0139a5f4d3b7297c7598cc921
-git apply ../patches/add-ip-phonebook.patch
+git checkout $GIT_BRANCH
+git apply ../patches/make-game-longer.patch
+# Not sure why this is needed in v1.1.0
 cp Packaging/resources/icon.png Packaging/resources/Diablo_32.png
 Packaging/OpenDingux/build.sh rg350
